9a62c052196d2a967f183cc7e8707c62f2b8f087
helenetrommelen <helenetrommelen@gmail.com>
2023-06-30 10:18:10 +0200

    added chatgpt functionality in js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+
 const loading = document.querySelector('.loading')
 const modal = document.querySelector('.modal')
 const modalContent = document.querySelector('.modal-content')
 const modalClose = document.querySelector('.modal-close')
-// const hidden = document.querySelector('.hidden');
+
 
 
 // move mouset to click: https://www.kirupa.com/snippets/move_element_to_click_position.htm
@@ -18,15 +19,80 @@ const modalClose = document.querySelector('.modal-close')
 //         loading.style.transform = translate3DValue;
 // }
 
+// get action for chatGPT
+function getRandomAction(){
+    const actions = [
+        'tell us how you determine whether something is true or not',
+        'what is your most important message for future generations',
+        'tell us what your main area of study is',
+        'what is good scientific work',
+        'what is your driving principle',
+        'write your linkedin bio'
+    ]
+    const randomIndex = Math.floor(Math.random() * actions.length)
+    return actions[randomIndex]
+}
+
+// Create a function that will run when we click on a character
+async function playPhilosopher(philosopher) {
+    // Remove the hidden class from the loading image so it shows on screen
+    loading.classList.remove('hidden');
+
+    // Define some content to display in the modal. We'll switch this out with AI dialog later!
+    const content = 'You clicked a character!'
+    // Get a random action by running the function we created
+    const action = getRandomAction();
+
+    // Send a prompt to ChatGPT
+    const response = await fetch(_CONFIG_.API_BASE_URL + '/chat/completions', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${_CONFIG_.API_KEY}`,
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            model: _CONFIG_.GPT_MODEL,
+            messages: [
+                {
+                    role: 'user',
+                    content: `You are ${philosopher} and should ${action} in a maximum of 100 characters without breaking character`
+                },
+            ]
+        })
+    })
+
+// async function playPhilosopher(philosopher) {
+//     loading.classList.remove('hidden')
+
+//     //sending the philosopher data and the prompt to chatGPT
+//     const action = getRandomAction()
+
+//     const response = await fetch(_CONFIG_.API_BASE_URL + '/chat/completions', {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer${_CONFIG_.API_KEY}`
+//         },
+//         method: 'POST',
+//         body: JSON.stringify({
+//             model: _CONFIG_.GPT_MODEL,
+//             messages: [
+//                 {
+//                     role: 'user',
+//                     content: `You are the philosopher ${philosopher} and should ${action} in a maximum of 100 characters without breaking character`
+//                 }
+//             ]
+//         })
+//     })
+
+    const jsonData = await response.json()
+    console.log(jsonData);
 
 
-
-function playPhilosopher(philosopher) {
-    //sending the philosopher data and the prompt to chatGPT
-    loading.classList.remove('hidden')
+    
+    
 
     //add content to modal
-    const content = 'you clicked a philosopher'
+    // const content = 'you clicked a philosopher'
     modalContent.innerHTML = `
         <h1>${philosopher}</h1>
         <p>${content}</p>
@@ -60,4 +126,4 @@ function init(){
 }
 
 init()
-moveBrainToMouseClick()
+// moveBrainToMouseClick()
